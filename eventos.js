509a8828798fb1c5096b1b69dcc8db6da29e57a4
Valentina Carrique <valentinacarriqueviera@gmail.com>
2024-08-21 18:44:23 -0300

    Cambios finales

---
+++ eventos.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+var miDiv = document.getElementById("miDiv");
+
+miDiv.addEventListener("click", function() {
+    alert("¡Hola! Soy el div");
+});
+
+
+});
