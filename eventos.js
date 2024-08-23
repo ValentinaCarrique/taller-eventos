d792ce3720a22823dddcb0a69872aa04ad8262f5
ValentinaCarrique <valentinacarriqueviera@gmail.com>
2024-08-23 10:46:57 -0300

    stopPropagation

--- eventos.js
+++ eventos.js
@@ -6,5 +6,11 @@ miDiv.addEventListener("click", function() {
     alert("¡Hola! Soy el div");
 });
 
+var miBoton = document.getElementById("miBoton");
 
+miBoton.addEventListener ("click", function (event){
+    alert ("¡Hola!");
+    event.stopPropagation();
+});
+    
 });
